0294c7f551b225103119d0d91a5c498e60f47315
Th5tch3r <amber.phan27@gmail.com>
2019-05-30 22:15:29 -0400

    added classes for lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,62 @@
 // CODE here for your Lambda Classes
+//Person Class
+class Person {
+    constructor(attr) {
+        this.name = attr.name;
+        this.age = attr.age;
+        this.location = attr.location;
+    }
+    speak() {
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
+}
+
+//Instructor 
+class Instructor extends Person {
+    constructor(insAttr) {
+        super(insAttr);
+        this.specialty = insAttr.specialty;
+        this.favLanguage = insAttr.favLanguage;
+        this.catchPhrase = insAttr.catchPhrase;
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}`;
+    }
+}
+
+//Student 
+class Student extends Person {
+    constructor(stuAttr) {
+        super(stuAttr);
+        this.previousBackground = stuAttr.previousBackground;
+        this.className = stuAttr.className;
+        this.favSubjects = stuAttr.favSubjects;
+    }
+    listsSubjects() {
+        this.favSubjects.map(items => console.log(items));
+    }
+    PRAssignment(subject){
+        return `${student.name} has submitted a PR for ${subject}`;
+    }
+    sprintChallenge(subject){
+        return `${student.name} has begun sprint challenge on ${subject}`;
+    }
+}
+
+//PM
+class ProjectManager extends Instructor {
+    constructor(pmAttr) {
+        super(pmAttr);
+        this.gradClassName = pmAttr.gradClassName;
+        this.favInstructor = pmAttr.favInstructor;
+    }
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standy times!`;
+    }
+    debugsCode (student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
+    }
+}
